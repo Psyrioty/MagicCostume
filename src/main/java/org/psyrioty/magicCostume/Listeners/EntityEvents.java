47aba735164658714d89ef627bd9e27348d20ba8
psyrioty <psyrioty@hotmail.com>
2026-06-25 16:17:44 +0300

    Fix teleports for other plugins

--- src/main/java/org/psyrioty/magicCostume/Listeners/EntityEvents.java
+++ src/main/java/org/psyrioty/magicCostume/Listeners/EntityEvents.java
@@ -6,10 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityTeleportEvent;
-import org.bukkit.event.player.PlayerChangedWorldEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.player.*;
 import org.psyrioty.magicCostume.Database.DatabaseManager;
 import org.psyrioty.magicCostume.Database.Requests;
 import org.psyrioty.magicCostume.MagicCostume;
@@ -109,7 +106,7 @@ public class EntityEvents implements Listener {
                 }
             });
             activeCostumeEntity.setRemake(false);
-        }, 5L);
+        }, 1L);
 
         for(ActiveSlot activeSlot: activeCostumeEntity.getActiveSlotList()){
             ActiveCostume activeCostume = activeSlot.getActiveCostume();
@@ -118,6 +115,93 @@ public class EntityEvents implements Listener {
             }
             activeCostume.remove();
         }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onCommand(PlayerCommandPreprocessEvent event) {
+        if(event.isCancelled()){
+            return;
+        }
+
+        Player player = event.getPlayer();
+        String message = event.getMessage();
+
+        if(
+                !message.startsWith("spawn") &&
+                !message.startsWith("warp") &&
+                !message.startsWith("home")
+        ){
+            return;
+        }
+
+        ActiveCostumeEntity activeCostumeEntity = MagicCostume.getPlugin().findActiveCostumeEntityForEntity(player);
+        if(activeCostumeEntity == null){
+            return;
+        }
+
+        if(activeCostumeEntity.isRemake()){
+            return;
+        }
+
+        activeCostumeEntity.setRemake(true);
+
+        for(ActiveSlot activeSlot: activeCostumeEntity.getActiveSlotList()){
+            ActiveCostume activeCostume = activeSlot.getActiveCostume();
+            if(activeCostume == null){
+                continue;
+            }
+            activeCostume.remove();
+        }
+
+        Bukkit.getScheduler().runTaskLater(MagicCostume.getPlugin(), () -> {
+            Bukkit.getScheduler().runTaskAsynchronously(MagicCostume.getPlugin(), () -> {
+                Connection connection = DatabaseManager.getConnection();
+                try (PreparedStatement ps = Requests.selectCostumePartsByEntityUUID(connection, player.getUniqueId().toString());
+                     ResultSet rs = ps.executeQuery()) {
+
+                    while (rs.next()) {
+                        int id = rs.getInt("id");
+                        String name = rs.getString("name");
+                        double scale = rs.getDouble("scale");
+                        int brightness = rs.getInt("brightness");
+                        double offsetX = rs.getDouble("offsetX");
+                        double offsetY = rs.getDouble("offsetY");
+                        double offsetZ = rs.getDouble("offsetZ");
+                        int slotId = rs.getInt("slot_id");
+
+
+                        boolean isHead = false;
+                        Costume cost = null;
+                        for(Costume costume: MagicCostume.getPlugin().getCostumes()){
+                            if(costume.getId().equals(name)){
+                                isHead = costume.isHeadModel();
+                                cost = costume;
+                            }
+                        }
+
+                        HashMap<UUID, Integer> boneBrightness = MagicCostume.getPlugin().setAllBoneBrightness(
+                                cost.getModel().getHeadBones(),
+                                null,
+                                brightness
+                        );
+
+                        MagicCostume.getPlugin().spawnActiveCostume(
+                                player,
+                                cost,
+                                boneBrightness,
+                                (float) scale,
+                                (float) offsetX,
+                                (float) offsetY,
+                                (float) offsetZ,
+                                isHead
+                        );
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                activeCostumeEntity.setRemake(false);
+            });
+        }, 1L);
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
@@ -190,7 +274,7 @@ public class EntityEvents implements Listener {
                 }
                 activeCostumeEntity.setRemake(false);
             });
-        }, 5L);
+        }, 1L);
     }
 
     @EventHandler
